299412e0e353542b127b2e1717870dc1a0bcd01b
Sohaib Mubashir <sohaibmh@yahoo.com>
2019-07-10 19:11:12 +0000

    Automatically backed up by Learn

--- spec/intro_to_ruby_hashes_lab_spec.rb
+++ spec/intro_to_ruby_hashes_lab_spec.rb
@@ -35,7 +35,7 @@ describe "working with hashes" do
 
     describe "my_hash_creator" do
       it "accepts a key and a value as parameters and returns a hash with this key/value pair inside" do
-
+        expect(my_hash_creator(:name, 'Grace Hopper')).to be_a(Hash)
         expect(my_hash_creator(:name, 'Grace Hopper')).to eq({name: 'Grace Hopper'})
 
         expect(my_hash_creator(1, 2)).to eq({1 => 2})
