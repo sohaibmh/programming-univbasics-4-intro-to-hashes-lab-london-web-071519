9444d7a4bb406a1c800d4ee17cc7ebcbebacca80
Sohaib Mubashir <sohaibmh@yahoo.com>
2019-07-10 19:12:26 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -16,7 +16,7 @@ end
 
 def my_hash_creator(key, value)
 
- a.each { |key, value| "#{key} #{value}" }
+ puts a.each { |key, value| "#{key} #{value}" }
   # return a hash that includes the key and value parameters passed into this method
 end
 
