d8708159f9fd0f920a44eee1fa025d02ca22da0b
Sohaib Mubashir <sohaibmh@yahoo.com>
2019-07-10 19:02:35 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -15,7 +15,7 @@ def id_generator
 end
 
 def my_hash_creator(key, value)
-a = {a: "all"}
+ a = {a: "all"}
  a.each { |key, value| "#{key} #{value}" }
   # return a hash that includes the key and value parameters passed into this method
 end
